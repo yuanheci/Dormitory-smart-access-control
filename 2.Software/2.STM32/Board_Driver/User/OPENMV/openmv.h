f45931cfe1f9615a5abea03c86fa9b42880daf70
ren-shihao <rsh15355756202@163.com>
2024-07-28 22:38:14 +0800

    Update openmv.h

--- 2.Software/2.STM32/Board_Driver/User/OPENMV/openmv.h
+++ 2.Software/2.STM32/Board_Driver/User/OPENMV/openmv.h
@@ -2,16 +2,18 @@
 #define __OPENMV_
 #include "sys.h"
 
-extern int Openmv_X;          /*OPENMV X Öá·´À¡×ø±ê*/
-extern int Openmv_Y;          /*OPENMV X Öá·´À¡×ø±ê*/
+extern int Openmv_X;          /*OPENMV X è½´åé¦ˆåæ ‡*/
+extern int Openmv_Y;          /*OPENMV X è½´åé¦ˆåæ ‡*/
 extern int Face_Inf;
 
 extern int openmv[5];
 extern int face_information[5];
 
 void Openmv_data(void);
-void Openmv_Recive(int data);
-void face_inf_Rec(int data);
+void Openmv_Recive(u8 q[]);
+void face_inf_Rec(u8 q[]);
 void Face_data(void);
+
+extern u8 q[100], hh, tt, maxn;
 
 #endif
